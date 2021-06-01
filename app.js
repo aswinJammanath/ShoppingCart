1e215473af897de9595b28b16eedf065c96909b4
aswinJammanath <aswinjammanath@gmail.com>
2021-06-01 18:07:35 +0530

    added user and admin pages, configured mongoDB, create collections and added data to the database, took data from the database to show it in user view

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var hbs = require('express-handlebars')
+var userRouter = require('./routes/user');
+var adminRouter = require('./routes/admin');
+var hbs = require('express-handlebars');
 var app = express();
+var fileupload = require('express-fileupload');
+var db = require('./config/connection')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +23,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileupload())
+db.connect((err)=>{
+if(err) console.log('Connection Error' + err)
+else console.log("Database Connected to port 27017")
+})
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ routes/user.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var productHelper = require('../helper/product-helper');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  
+  productHelper.getAllProducts().then((products)=>{
+    res.render('user/view-products', { admin: false, products });
+
+  })
+});
+
+module.exports = router;
